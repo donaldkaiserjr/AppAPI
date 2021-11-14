5ebf4a30c7cfcc117c6d7b0ba1d4ab175d78d486
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-11-13 16:05:09 -0800

    AppForm2.js update Add AppForm2.js file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 // import App from "./App";
 import AppAPI from "./APIs/AppAPI";
 import AppForm from "./APIs/AppForm";
+import AppForm2 from "./APIs/AppForm2";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
+    <AppForm2 />
     <AppAPI />
     <AppForm />
     {/* <App /> */}
